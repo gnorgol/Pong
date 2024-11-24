05913a4e643c93f5ba151d5fba36dc6bc1fca6f7
Guillaume Norgol <52493523+gnorgol@users.noreply.github.com>
2024-11-24 10:58:39 +0100

    Ajout du système d'entrée Unity Input System
    Introduction du nouveau système d'entrée Unity Input System dans le projet.
    - Ajout de `com.unity.inputsystem` version `1.11.2` dans `manifest.json`.
    - Mise à jour de `packages-lock.json` pour inclure les dépendances.
    - Modification de `ProjectSettings.asset` pour changer `activeInputHandler` de `0` à `2`.
    - Ajout d'un dossier `Input` avec un fichier `meta`.
    - Ajout de `InputAction.inputactions` pour définir les actions de jeu.
    - Ajout de fichiers `meta` pour `InputAction.inputactions`.
    - Ajout de nouveaux `GameObjects` (`VirtualInput`, `JoyStick`) dans `SampleScene.unity`.
    - Ajout et modification de `RectTransform` et `MonoBehaviour` dans `SampleScene.unity`.

--- Assets/Script/Paddle.cs
+++ Assets/Script/Paddle.cs
@@ -1,13 +1,28 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Paddle : MonoBehaviour
 {
     public float speed = 10f;
-   
+    public InputActionReference moveAction;
+
+
+
+    private void OnEnable()
+    {
+        moveAction.action.Enable();
+    }
+
+    private void OnDisable()
+    {
+        moveAction.action.Disable();
+    }
 
     private void Update()
     {
-        float move = Input.GetAxisRaw("Vertical") * speed * Time.deltaTime;
+        // Déplacement de la raquette
+        Vector2 moveInput = moveAction.action.ReadValue<Vector2>();
+        float move = moveInput.y * speed * Time.deltaTime;
         transform.Translate(0, move, 0);
 
         // Limiter le mouvement des raquettes à l'écran
